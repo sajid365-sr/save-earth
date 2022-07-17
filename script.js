a70b6bfac03c95f4f171d16bd6f8ff3f20b7b825
sajid365-sr <sajidsorker86@gmail.com>
2022-07-18 01:21:56 +0600

    js added

---
+++ script.js
@@ -0,0 +1,23 @@
+function donateNow(){
+    window.scrollBy(0,548);
+    
+}
+
+
+let tk = document.querySelectorAll('.tkAmount>button>h2');
+let input = document.getElementById('donateInput');
+
+for(let keys of tk){
+    keys.addEventListener('click',function(){
+        let amntValue = keys.innerHTML;
+     input.value = amntValue;
+    
+        
+        })
+}
+
+let btn = document.getElementById('secondBtn');
+
+btn.addEventListener('click',function(){
+    window.open('https://sajid365-sr.github.io/developer-portfolio/','payment','top=100,left=400,height=400px,width=500px');
+})
